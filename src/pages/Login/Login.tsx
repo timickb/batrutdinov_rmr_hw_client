b8af8f42682bb045b4f3eb6c46b57fc79c5eea48
timickb <timickb@yanex.ru>
2022-08-23 00:52:41 +0500

    dev: changed Login title

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
     const { setHeader } = useContext(HeaderCtx);
     const authStore = useContext(AuthCtx);
 
-    useEffect(() => setHeader('Authorization'), []);
+    useEffect(() => setHeader('Signing in'), []);
 
     const [data, setData] = useState({} as FormData)
     const [error, setError] = useState({} as FormDataError)
