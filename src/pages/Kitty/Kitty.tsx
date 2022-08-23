1ed4e04aa49ca538b1fe0a8a156744440746678f
timickb <timickb@yanex.ru>
2022-08-23 17:46:13 +0500

    dev: adaptive kitty

--- src/pages/Kitty/Kitty.tsx
+++ src/pages/Kitty/Kitty.tsx
@@ -1,15 +1,40 @@
 import { HeaderCtx } from "@/App";
 import Loader from "@/components/Loader/Loader";
-import { getKitty, KittyResponse } from "@/infrastructure/http";
+import { getKitty } from "@/infrastructure/http";
 import React, { useEffect, useState, useContext } from "react";
+
+
+const min = (a: number, b: number) : number => {
+    return a < b ? a : b;
+}
 
 export default function Kitty() {
     const {setHeader} = useContext(HeaderCtx);
-    useEffect(() => setHeader('Kitty'), []);
-
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(false);
     const [path, setPath] = useState('');
+
+    const [dimension, setDimension] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight
+    });
+
+    const handleChangeDimension = () => {
+        setDimension({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
+
+    useEffect(() => setHeader('Kitty'), []);
+    useEffect(() => {
+        window.addEventListener('resize', handleChangeDimension);
+        return () => {
+            window.removeEventListener('resize', handleChangeDimension);
+        }
+    }, [dimension]);
+
+    
 
     useEffect(() => {
         setLoading(true);
@@ -25,7 +50,10 @@ export default function Kitty() {
         <div style={{borderRadius: 24, overflow: 'hidden'}}>
             {loading ? <Loader /> : (
                 success 
-                ? <img src={path} width={400} height={400} />
+                ? <img 
+                    src={path} 
+                    width={min(400, dimension.width - 16)} 
+                    height={min(400, dimension.width - 16)} />
                 : <h3>Forbidden</h3>
             )}
         </div>
