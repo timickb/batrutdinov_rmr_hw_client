80e523a56225a31e40794dfc0a3d12886f1dfc6c
timickb <timickb@yanex.ru>
2022-08-23 17:06:34 +0500

    dev: responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,16 @@ import "@/components/Footer/Footer.css";
 export default function Footer() {
     return (
         <div className="footer">
-            <div>&copy; 2022 Kitty</div>
-            <a href="terms" className="footer-menu-item">Terms</a>
-            <a href="privacy" className="footer-menu-item">Privacy</a>
-            <a href="security" className="footer-menu-item">Security</a>
-            <a href="api" className="footer-menu-item">API</a>
-            <a href="about" className="footer-menu-item">About</a>
+            <div className="footer-left">
+                <div>&copy; 2022 Kitty</div>
+            </div>
+            <div className="footer-right">
+                <a href="terms" className="footer-menu-item">Terms</a>
+                <a href="privacy" className="footer-menu-item">Privacy</a>
+                <a href="security" className="footer-menu-item">Security</a>
+                <a href="spinner" className="footer-menu-item">Spinner</a>
+                <a href="about" className="footer-menu-item">About</a>
+            </div>
         </div>
     )
 }
